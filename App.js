244a6681351c803736818928625cf568276eb520
Abi Andrea Nurpasha <110515353+KingAbby@users.noreply.github.com>
2025-04-28 22:41:13 +0700

    Tailwind + App
    Setup Tailwind + App

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import Tester from "./pages/tester";
+import Hello from "./pages/hello";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-blue-300">
+      <Tester />
+      <Hello />
     </View>
   );
 }
